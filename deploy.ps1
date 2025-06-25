52080c4487d81a404f186d84a9a35b3d74451b7e
hoaiphong97 <111399979+hoaiphong97@users.noreply.github.com>
2025-06-25 16:53:57 +0700

    Delete deploy.ps1

--- deploy.ps1
+++
@@ -1,9 +0,0 @@
-param (
-    [string]$PackageName
-)
-
-Write-Host "Deploying package: $PackageName"
-
-# Simulate deployment logic
-Start-Sleep -Seconds 2
-Write-Host "Deployment for $PackageName completed"
